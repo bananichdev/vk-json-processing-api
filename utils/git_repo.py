74dd988d54bbb9fa320005a42a071de98faef5d8
bananichdev <dimitri-05@mail.ru>
2024-07-20 13:56:16 +0300

    master

--- utils/git_repo.py
+++ utils/git_repo.py
@@ -8,7 +8,7 @@ def git_commit_and_tag(tag: str, message: str):
     repo.git.add(update=True)
     repo.index.commit(message)
     repo.create_tag(tag)
-    repo.git.push("master", "main")
+    repo.git.push("master")
     repo.git.push("master", tag)
 
 
